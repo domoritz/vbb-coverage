e8f3497588aacc6ba8fbe5fc1e0cc0721fa915cf
Dominik Moritz <domoritz@gmail.com>
2013-03-21 22:21:24 +0100

    Use heatmap instead of mask canvas

--- static/js/main.js
+++ static/js/main.js
@@ -25,13 +25,40 @@ $(function() {
 
     var coverageLayer = new L.TileLayer.MaskCanvas({'opacity': 0.5, radius: 750, useAbsoluteRadius: true, 'attribution': 'Get the data at <a href="//daten.berlin.de/datensaetze/vbb-fahrplan2012">daten.berlin.de</a>. Code on <a href="//github.com/domoritz/vbb-coverage">Github</a>'});
 
+    var heatmapLayer = L.TileLayer.heatMap({
+        // radius could be absolute or relative
+        // absolute: radius in meters, relative: radius in pixels
+        radius: { value: 800, absolute: true },
+        //radius: { value: 20, absolute: false },
+        opacity: 0.8,
+        gradient: {
+            0.45: "rgb(0,0,255)",
+            0.55: "rgb(0,255,255)",
+            0.65: "rgb(0,255,0)",
+            0.95: "yellow",
+            1.0: "rgb(255,0,0)"
+        }
+    });
+
     var loadOverlay = function(id) {
         var url = 'data/' + id + '.json';
         $.getJSON(url).success(function(data) {
-            coverageLayer.setData(data);
+            //coverageLayer.setData(data);
             //L.rectangle(coverageLayer.bounds, {color: "#ff7800", weight: 1}).addTo(map);
-            map.fitBounds(coverageLayer.bounds);
-            map.addLayer(coverageLayer);
+            //map.fitBounds(coverageLayer.bounds);
+            //map.addLayer(coverageLayer);
+            this.bounds = new L.LatLngBounds(data);
+
+            converted = [];
+            for (var i = data.length - 1; i >= 0; i--) {
+                converted.push({
+                    value: 1,
+                    lat: data[i][0],
+                    lon: data[i][1]
+                });
+            }
+            heatmapLayer.setData(converted);
+            map.addLayer(heatmapLayer);
         }).error(function(err) {
             alert('An error occurred', err);
         });
